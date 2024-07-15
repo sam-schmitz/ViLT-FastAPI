5575eee6018af6e48063e4658b31384fa59aefab
sam-schmitz <116108955+sam-schmitz@users.noreply.github.com>
2024-07-15 14:38:13 -0500

    made the ask method for the api. This method uses the model in model.py

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
 #main.py
+#The FastAPI app for the model
 
 from model import model_pipeline
 
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile
+import io
+from PIL import Image
 
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.post("/ask")
+def ask(text: str, image: UploadFile):
+    #grab the image from the upload file
+    content = image.file.read()     
+    
+    image = Image.open(io.BytesIO(content))
+    #image = Image(image.file)
+    
+    result = model_pipeline(text, image)
+    
+    return result
